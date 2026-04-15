450ae4d82625533d745c05bf63f1e46d0aa95883
Nji Dimitri. K <dimitrinji@gmail.com>
2026-04-15 11:56:30 -0400

    Added queries of actors and film tables

--- Week-2/Ex1B_first_query.sql
+++ Week-2/Ex1B_first_query.sql
@@ -1,3 +1,7 @@
 /*
 Sakila is a database created for learnig and practice on how databases are structured and also to practice writing SQL quaries 
 */
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 records
+
+SELECT * FROM film; -- Retrieved 1000 records
