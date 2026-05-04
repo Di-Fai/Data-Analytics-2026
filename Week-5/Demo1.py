34ea623d7cc3bf8ac6da078c5595632b4e122979
Nji Dimitri. K <dimitrinji@gmail.com>
2026-05-04 16:03:02 -0400

    Class Demo

---
+++ Week-5/Demo1.py
@@ -0,0 +1,17 @@
+# Dimitri K. Nji
+# 5/4/2026
+
+name = "Dimitri"
+age = 26
+
+print(name)
+print(age)
+
+print(f'my name is {name} and i am {age} years old')
+
+pi = 3.145678
+
+print(f"The value of pi is {pi:.2f}")
+
+sale = 12345.7685
+print(f"The value of sale is ${sale:,.2f}")
