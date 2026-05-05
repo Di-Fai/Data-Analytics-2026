d4f355567756da8304a5081730bbd41d589c9f4b
Nji Dimitri. K <dimitrinji@gmail.com>
2026-05-05 10:40:21 -0400

    varaibles

--- Week-5/EX1B_varaible_examples.py
+++ Week-5/EX1B_varaible_examples.py
@@ -1,10 +1,10 @@
 customer_id = 1001  # I used a number because customer IDs are usually numeric, but it could also be text if it includes letters.
 
-customer_name = "Dimitri Nji"  # I wrote first name + last name together, but it could be better as separate first_name and last_name variables.
+customer_name = "Dominique Orcha"  # I wrote first name + last name together, but it could be better as separate first_name and last_name variables.
 
 customer_gender = "Male"  # I used text, but the system may need specific options like M, F, or Other.
 
-customer_date_of_birth = "1998-05-15"  # I used text in YYYY-MM-DD format, but Python also has date formats for more advanced use.
+customer_date_of_birth = "1998-03-18"  # I used text in YYYY-MM-DD format, but Python also has date formats for more advanced use.
 
 drivers_license_number = "D123456789"  # I used text because license numbers can include letters and numbers.
 
@@ -12,6 +12,10 @@ auto_policy_number = "AUTO987654"  # I used text because policy numbers can incl
 
 my_name = "Dimitri Nji"  # I used my full name as one text value.
 
-hometown = "New Castle, Delaware"  # I used city and state together, but they could be separated into city and state variables.
+hometown = "Douala, Litoral"  # I used city and region together, but they could be separated into city and region variables.
+
+
+
+
 
 
