45e3ab891e622264827c2e951bea67ce9df1ba45
Nji Dimitri. K <dimitrinji@gmail.com>
2026-05-06 14:46:57 -0400

    Class exe

---
+++ Week-5/Reciept Class Exe.py
@@ -0,0 +1,12 @@
+meal_cost = float(input("Enter the cost of the meal: $"))
+
+tip = meal_cost * 0.20
+tax = meal_cost * 0.0825
+total_cost = meal_cost + tip + tax
+
+print("----- Meal Cost Breakdown -----")
+print(f"{'Meal Cost:':<15} ${meal_cost:>6.2f}")
+print(f"{'Tip (20%):':<15} ${tip:>6.2f}")
+print(f"{'Tax (8.25%):':<15} ${tax:>6.2f}")
+print("-------------------------------")
+print(f"{'Total Cost:':<14} ${total_cost:>7.2f}")
