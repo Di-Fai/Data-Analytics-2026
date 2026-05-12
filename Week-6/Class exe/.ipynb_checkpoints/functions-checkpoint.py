8c262ae5772f00b3ed4ff01bacabc8f5dcd1eabf
Nji Dimitri. K <dimitrinji@gmail.com>
2026-05-12 15:24:49 -0400

    updates

---
+++ Week-6/Class exe/.ipynb_checkpoints/functions-checkpoint.py
@@ -0,0 +1,10 @@
+# Dimitri Nji
+# 5/12/2026
+# demonstrates a return statement 
+
+def greeting():
+    name = input("Please enter your name: ")
+    return name
+
+result = greeting()
+print(f"Hello, {result}!")
